3c89143fed1df32af3f0cd43ede44c763466dff4
cdrvietnam-tech <cdrvietnam@gmail.com>
2026-09-18 09:29:49 +0700

    [Agent Evolution] Publish September 18 label guides

--- content/blog/index.ts
+++ content/blog/index.ts
@@ -11,12 +11,14 @@ import article8 from "./published/tem-chai-serum.json";
 import article9 from "./published/tem-cho-hu-kem.json";
 import article10 from "./published/tem-chong-nuoc-cho-my-pham-va-do-uong.json";
 import article11 from "./published/tem-giay-va-tem-nhua-nen-chon-loai-nao.json";
-import article12 from "./published/tem-ly-ca-phe-chong-nuoc.json";
-import article13 from "./published/tem-nhan-trung-thu-hop-qua.json";
-import article14 from "./published/tem-nhua-trong-cho-chai.json";
-import article15 from "./published/tem-thuc-pham-cho-tui-kraft.json";
-import article16 from "./published/tem-uv-dtf-la-gi.json";
-import article17 from "./published/thu-tem-trong-moi-truong-lanh.json";
+import article12 from "./published/tem-hologram-7-mau.json";
+import article13 from "./published/tem-ly-ca-phe-chong-nuoc.json";
+import article14 from "./published/tem-nhan-trung-thu-hop-qua.json";
+import article15 from "./published/tem-nhieu-vi-san-pham.json";
+import article16 from "./published/tem-nhua-trong-cho-chai.json";
+import article17 from "./published/tem-thuc-pham-cho-tui-kraft.json";
+import article18 from "./published/tem-uv-dtf-la-gi.json";
+import article19 from "./published/thu-tem-trong-moi-truong-lanh.json";
 
 export const rawBlogArticles = [
   article0,
@@ -37,4 +39,6 @@ export const rawBlogArticles = [
   article15,
   article16,
   article17,
+  article18,
+  article19,
 ] as const;
